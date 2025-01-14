b6d0175236cb53d8ce88ee8d7704f2cabbdf71d0
bruno <bweisz@eryxsoluciones.com.ar>
2025-01-14 16:38:47 -0300

    refactor in files

--- sumTestsHalo/src/tests/mod.rs
+++ sumTestsHalo/src/tests/mod.rs
@@ -1,6 +1,9 @@
 use super::*;
 use halo2_proofs::halo2curves::bn256::Fr;
 
+mod tests_addition_mod_64;
+mod tests_rotation;
+
 fn max_u64() -> Value<Fr> {
     Value::known(Fr::from(((1u128 << 64) - 1) as u64))
 }
@@ -13,23 +16,11 @@ fn one() -> Value<Fr> {
 fn zero() -> Value<Fr> {
     Value::known(Fr::ZERO)
 }
-fn two_power_48() -> Value<Fr> {
-    Value::known(Fr::from(1 << 48))
-}
-
-mod tests_addition_mod_64;
-mod tests_rotation;
+fn two_power_48() -> Value<Fr> { Value::known(Fr::from(1 << 48)) }
 
 fn valid_addition_trace() -> [[Value<Fr>; 6]; 3] {
     [
-        [
-            max_u64(),
-            max_u16(),
-            max_u16(),
-            max_u16(),
-            max_u16(),
-            zero(),
-        ],
+        [max_u64(), max_u16(), max_u16(), max_u16(), max_u16(), zero()],
         [one(), one(), zero(), zero(), zero(), zero()],
         [zero(), zero(), zero(), zero(), zero(), one()],
     ]
