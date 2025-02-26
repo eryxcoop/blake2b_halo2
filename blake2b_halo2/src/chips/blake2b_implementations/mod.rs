006d26520859f7a54b7e2f56d77dea0c2f67ff98
bruno <bweisz@eryxsoluciones.com.ar>
2025-02-26 17:46:20 -0300

    Separated the implementations from the parameterized version of the blake2b chip

--- blake2b_halo2/src/chips/blake2b_implementations/mod.rs
+++ blake2b_halo2/src/chips/blake2b_implementations/mod.rs
@@ -1,3 +1,9 @@
 use super::*;
 
+/// This is the chip with all the optimizations, toggled with features
 pub mod blake2b_chip;
+
+/// These are the separated optimizations, they exist with benchmarking purposes
+pub mod blake2b_chip_a;
+pub mod blake2b_chip_b;
+pub mod blake2b_chip_c;
