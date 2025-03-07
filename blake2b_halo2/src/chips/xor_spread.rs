330863c1c6267fcd12c2d6818d4306714b648d6d
ChinoCribioli <ezequiel.cribioli@gmail.com>
2025-03-07 11:58:23 -0300

    fixed import warnings

--- blake2b_halo2/src/chips/xor_spread.rs
+++ blake2b_halo2/src/chips/xor_spread.rs
@@ -1,6 +1,6 @@
 use std::array;
 use halo2_proofs::circuit::AssignedCell;
-use crate::auxiliar_functions::{value_for};
+use crate::auxiliar_functions::value_for;
 use crate::chips::decompose_8::Decompose8Config;
 use super::*;
 
