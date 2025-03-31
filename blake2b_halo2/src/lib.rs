bfb67cb12d50df07a9db4956fee8deecfb9eeedf
fdecroix <fdecroix@eryx.co>
2025-03-31 13:59:55 -0300

    Only make public what is necessary in lib.rs

--- blake2b_halo2/src/lib.rs
+++ blake2b_halo2/src/lib.rs
@@ -7,11 +7,11 @@ use halo2_proofs::circuit::{Region, Value};
 use halo2_proofs::plonk::{Advice, Column, Error, Expression, Selector, TableColumn};
 use halo2_proofs::poly::Rotation;
 
-pub mod auxiliar_functions;
-pub mod base_operations;
+pub(crate) mod auxiliar_functions;
+pub(crate) mod base_operations;
 
 #[cfg(test)]
-pub mod tests;
-pub mod blake2b;
+pub(crate) mod tests;
+pub(crate) mod blake2b;
 pub mod example_blake2b_circuit;
 mod types;
